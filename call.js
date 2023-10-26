05b45a8e0071b3efb089b80ef38a23ea0268bb73
Caasi <47527704+undependable@users.noreply.github.com>
2023-10-26 12:48:26 +0200

    Update call.js
    Added an input field clear, so whenever a guess is wrong or right, the input field will be cleared
    
    Added a new point implementation
    
    Removed the "var" in the variable "revealed", and is now using "let" instead to remove old javascript typings.

--- call.js
+++ call.js
@@ -1,34 +1,39 @@
 let currentPokemonName = ""; // Track the name of the current Pokemon
 let points = 0; // Assign the start point value as 0
 let totalHints = 1; // Assign the totalHints used as 0
-var revealed = [] // Assign the empty list where it'll be used later on to add individual letters of the pokemon per hint 
+let revealed = [] // Assign the empty list where it'll be used later on to add individual letters of the pokemon per hint 
 
-document.getElementById("press").addEventListener("click", guessPokemon, true);
+
+// Making the button functional and returning a function back
+document.getElementById("press").addEventListener("click", guessPokemon, true); 
 document.getElementById("reset").addEventListener("click", randomPokemon, true);
 document.getElementById("hint").addEventListener("click", hint, true);
 
 // Function to generate a random Pokémon
 function randomPokemon() {
     let min = 1; // Minimum Pokémon number
-    let max = 800; // Maximum Pokémon number
+    let max = 899; // Maximum Pokémon number
     let randomPokemonNumber = Math.floor(Math.random() * (max - min + 1)) + min; // Generate a random Pokémon number
+    
     let url = "https://pokeapi.co/api/v2/pokemon/" + randomPokemonNumber; // Construct the URL
     apiRequest(url); // Send the url result so that the function "apiRequest" can use it
 
     revealed = [] // Reset the list
     totalHints = 1 // Reset totalHints
-    document.getElementById("revealed").innerHTML = ""
+    
+    document.getElementById("revealed").innerHTML = "" // Reset the hinted letters
+    document.getElementById("guess").value = ""  // Reset the guessig input text
 }
 
 // Function to make an API request to retrieve Pokémon data
 function apiRequest(url) {
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        // If response code is 200
+    let httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function() {
+        // If response code is 200 and the client state is equal to 4
         if (this.readyState == 4 && this.status == 200) {
-            let responseData = xhttp.responseText;
-            let parsedData = JSON.parse(responseData);
-            let pokemonName = parsedData.name;
+            let responseData = httpRequest.responseText; // Get the response text
+            let parsedData = JSON.parse(responseData); // Parsing it into a JSON dict
+            let pokemonName = parsedData.name; // Res into the keys to get the pokemon name key
 
             // Access the sprites array and get the front_default image URL and then update it
             let spriteModel = parsedData.sprites.other.home.front_default;
@@ -42,8 +47,8 @@ function apiRequest(url) {
 
 
     // Make the API request through GET-request
-    xhttp.open("GET", url, true);
-    xhttp.send();
+    httpRequest.open("GET", url, true);
+    httpRequest.send();
 }
 
 // Function to handle the player's guess
@@ -58,11 +63,19 @@ function guessPokemon() {
 
     // If the guess is correct, add +1 point to the currentPoints
     if (guess.toLowerCase() === currentPokemonName.toLowerCase()) {
+        // Display to the user that their answer was correct.
         displayText.innerHTML = ("Correct! You guessed the Pokemon!");
         displayText.style.color = "green";
 
         // Add points per corrected guess
-        points += 1;
+        points += 3;
+
+        // Deduct points if the user have used more hints
+        if(totalHints <= 3){
+            points -= 2
+        }
+
+        // Refresh the HTML text for id, points and revealed.
         document.getElementById("points").innerHTML = "Current points: " + points;
         document.getElementById("revealed").innerHTML = ""
 
@@ -74,19 +87,17 @@ function guessPokemon() {
         randomPokemon(); 
 
         // Set a timeout to clear the message after 5 seconds
-        setTimeout(function () {
-            displayText.innerHTML = "";
-        }, 5000);
-
+        setTimeout(function () {displayText.innerHTML = "";}, 5000);
+        document.getElementById("guess").value = "" 
         
     } else {
+        // Display to the user that their answer was wrong.
         displayText.innerHTML = ("Wrong! Guess again!");
         displayText.style.color = "red";
 
         // Set a timeout to clear the message after 5 seconds
-        setTimeout(function () {
-            displayText.innerHTML = "";
-        }, 5000);
+        setTimeout(function () {displayText.innerHTML = "";}, 5000);
+        document.getElementById("guess").value = "" 
     }
 }
 
